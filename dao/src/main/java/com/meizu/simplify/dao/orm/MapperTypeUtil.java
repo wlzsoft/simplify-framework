b694742994cc8a40b0befe22a31f2f7faa7e7a0f
lcy <547393607@qq.com>
2016-08-09 11:03:55 +0800

    提供数据库和java类型匹配工具类

---
+++ dao/src/main/java/com/meizu/simplify/dao/orm/MapperTypeUtil.java
@@ -0,0 +1,37 @@
+package com.meizu.simplify.dao.orm;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+/**
+  * <p><b>Title:</b><i>数据库类型和java数据类型匹配工具类</i></p>
+ * <p>Desc: TODO </p>
+ * <p>source folder:{@docRoot}</p>
+ * <p>Copyright:Copyright(c)2014</p>
+ * <p>Company:meizu</p>
+ * <p>Create Date:2016年4月26日 上午11:35:50</p>
+ * <p>Modified By:luchuangye-</p>
+ * <p>Modified Date:2016年4月26日 上午11:35:50</p>
+ * @author <a href="mailto:luchuangye@meizu.com">luchuangye</a>
+ * @version Version 0.1
+ *
+ */
+public class MapperTypeUtil{
+	
+	/**
+	 * 
+	 * 方法用途: 匹配数据库字段类型和实体属性类型，并转换成实体类型<br>
+	 * 操作步骤: TODO<br>
+	 * @param val
+	 * @return
+	 */
+	public static Class<?> mapperOrmType(Object val) {
+		Class<?> valClazz = val.getClass();
+		if(valClazz == Timestamp.class) {
+			valClazz = Date.class;
+		} else if(valClazz == java.sql.Date.class) {
+			valClazz = Date.class;
+		}
+		return valClazz;
+	}
+}
