afd20df279effefae8d54a5fa27d8f540994f874
lcy <547393607@qq.com>
2016-08-24 12:51:18 +0800

    1.2.0开发版

--- utils/src/main/java/com/meizu/simplify/Constants.java
+++ utils/src/main/java/com/meizu/simplify/Constants.java
@@ -13,6 +13,6 @@ package com.meizu.simplify;
  *
  */
 public class Constants {
-	public static final String version = "v1.1.3.0";
+	public static final String version = "v1.2.0-SNAPSHOT";
 	public static final String packagePrefix = "com.meizu";
 }
