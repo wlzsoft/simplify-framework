d8e5a2af76425fbc3d8296f9c22f47c2d3677999
lcy <547393607@qq.com>
2016-11-04 18:42:39 +0800

    通过事务开启与否来做主从分离。

---
+++ datasource/src/test/java/com/meizu/simplify/datasource/DataSourceTest.java
@@ -0,0 +1,28 @@
+package com.meizu.simplify.datasource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.meizu.simplify.dao.datasource.DruidPoolFactory;
+import com.meizu.simplify.test.SimplifyJUnit4ClassRunner;
+
+@RunWith(SimplifyJUnit4ClassRunner.class)
+public class DataSourceTest {
+	@Test
+	public void test() {
+		Connection conn = DruidPoolFactory.getConnection();
+		try {
+			boolean isAutoCommit = conn.getAutoCommit();
+			if(isAutoCommit) {
+//				insert
+			} else {
+				//select
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
