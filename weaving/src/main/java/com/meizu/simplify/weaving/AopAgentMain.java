9d794e7b0a8a579f9c76c7da992dc5b422bbc04c
lcy <547393607@qq.com>
2016-11-09 16:26:37 +0800

    release1.2.0稳定版发布

--- weaving/src/main/java/com/meizu/simplify/weaving/AopAgentMain.java
+++ weaving/src/main/java/com/meizu/simplify/weaving/AopAgentMain.java
@@ -4,7 +4,7 @@ import java.lang.instrument.Instrumentation;
 
 /**
  * <p><b>Title:</b><i>字节码编辑植入处理类</i></p>
- * <p>Desc: java命令启用的参数： -javaagent:E:/workspace-new/simplify-framework/weaving/target/weaving-1.2.0-SNAPSHOT.jar -Daop.properties=E:/workspace-new/simplify-framework/aop/src/main/resources/aop.properties</p>
+ * <p>Desc: java命令启用的参数： -javaagent:E:/workspace-new/simplify-framework/weaving/target/weaving-1.2.0.jar -Daop.properties=E:/workspace-new/simplify-framework/aop/src/main/resources/aop.properties</p>
  * <p>source folder:{@docRoot}</p>
  * <p>Copyright:Copyright(c)2014</p>
  * <p>Company:meizu</p>
