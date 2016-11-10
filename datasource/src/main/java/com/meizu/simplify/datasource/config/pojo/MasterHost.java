661748f8ddd20d199a0c7438af0747cc23203500
lcy <547393607@qq.com>
2016-11-10 11:57:11 +0800

    datasource模块：添加主从配置相关类

---
+++ datasource/src/main/java/com/meizu/simplify/datasource/config/pojo/MasterHost.java
@@ -0,0 +1,69 @@
+package com.meizu.simplify.datasource.config.pojo;
+
+import java.util.List;
+
+/**
+ * <p><b>Title:</b><i>TODO</i></p>
+ * <p>Desc: TODO</p>
+ * <p>source folder:{@docRoot}</p>
+ * <p>Copyright:Copyright(c)2014</p>
+ * <p>Company:meizu</p>
+ * <p>Create Date:2016年11月10日 上午11:55:53</p>
+ * <p>Modified By:luchuangye-</p>
+ * <p>Modified Date:2016年11月10日 上午11:55:53</p>
+ * @author <a href="mailto:luchuangye@meizu.com" title="邮箱地址">luchuangye</a>
+ * @version Version 0.1
+ *
+ */
+public class MasterHost {
+	
+    private String name;
+    
+    private String url;
+    
+    private String userName;
+    
+    private String password;
+    
+    private List<SlaveHost> slaveHostList;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+	public List<SlaveHost> getSlaveHostList() {
+		return slaveHostList;
+	}
+
+	public void setSlaveHostList(List<SlaveHost> slaveHostList) {
+		this.slaveHostList = slaveHostList;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
---
+++ datasource/src/test/java/com/meizu/simplify/datasource/config/HostRouteConfigResolverForYamlTest.java
@@ -0,0 +1,78 @@
+package com.meizu.simplify.datasource.config;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.yaml.snakeyaml.Yaml;
+
+import com.meizu.simplify.datasource.config.pojo.HostSwitch;
+import com.meizu.simplify.datasource.config.pojo.MasterHost;
+import com.meizu.simplify.datasource.config.pojo.SlaveHost;
+
+/**
+ * <p><b>Title:</b><i>TODO</i></p>
+ * <p>Desc: TODO</p>
+ * <p>source folder:{@docRoot}</p>
+ * <p>Copyright:Copyright(c)2014</p>
+ * <p>Company:meizu</p>
+ * <p>Create Date:2016年11月10日 上午11:56:15</p>
+ * <p>Modified By:luchuangye-</p>
+ * <p>Modified Date:2016年11月10日 上午11:56:15</p>
+ * @author <a href="mailto:luchuangye@meizu.com" title="邮箱地址">luchuangye</a>
+ * @version Version 0.1
+ *
+ */
+public class HostRouteConfigResolverForYamlTest {
+	
+	@Test
+    public  void test1() {
+        try {
+//        	while(true) 
+        	{
+	            Yaml yaml = new Yaml();
+	            URL url = HostRouteConfigResolverForYamlTest.class.getClassLoader().getResource("host-switch.yaml");
+	            if (url != null) {
+	                Object obj = yaml.load(new FileInputStream(url.getFile()));
+	                System.out.println(obj);
+	            }
+//	            TimeUnit.SECONDS.sleep(5);
+        	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void test2() {
+        try {
+            HostSwitch h1 = new HostSwitch();
+            h1.setName("host1");
+            MasterHost masterHost = new MasterHost();
+            masterHost.setName("master1");
+            masterHost.setUrl("localhost:3306");
+            masterHost.setUserName("root");
+            masterHost.setPassword("123456");
+            List<SlaveHost> slaveHostList = new ArrayList<>();
+            SlaveHost slaveHost = new SlaveHost();
+            slaveHost.setName("slave1");
+            slaveHost.setUrl("localhost:8087");
+            slaveHost.setUserName("root");
+            slaveHost.setPassword("");
+            slaveHostList.add(slaveHost);
+            masterHost.setSlaveHostList(slaveHostList);
+            List<MasterHost> masterHostList = new ArrayList<>();
+            masterHostList.add(masterHost);
+            h1.setMasterHostList(masterHostList);
+            Yaml yaml = new Yaml();
+            yaml.dump(h1, new FileWriter("hostSwitchList.yaml"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        test2();
+    }
+}
