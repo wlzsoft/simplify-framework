9f4844a0b4a8e5ca6492c74e06ced49b93595558
lcy <547393607@qq.com>
2016-11-25 13:58:00 +0800

    新增config-plugins模块：目前用于支持配置服务化

---
+++ config-plugins/src/main/java/com/meizu/simplify/config/client/ConfigClientAnnotationResolver.java
@@ -0,0 +1,29 @@
+package com.meizu.simplify.config.client;
+
+import java.util.List;
+
+import com.meizu.simplify.ioc.annotation.Init;
+import com.meizu.simplify.ioc.enums.InitTypeEnum;
+import com.meizu.simplify.ioc.resolver.IAnnotationResolver;
+
+/**
+  * <p><b>Title:</b><i>配置服务客户端模块启用解析</i></p>
+ * <p>Desc: TODO</p>
+ * <p>source folder:{@docRoot}</p>
+ * <p>Copyright:Copyright(c)2014</p>
+ * <p>Company:meizu</p>
+ * <p>Create Date:2016年1月6日 上午11:08:36</p>
+ * <p>Modified By:luchuangye-</p>
+ * <p>Modified Date:2016年1月6日 上午11:08:36</p>
+ * @author <a href="mailto:luchuangye@meizu.com" >luchuangye</a>
+ * @version Version 0.1
+ *
+ */
+@Init(InitTypeEnum.CONFIG)
+public class ConfigClientAnnotationResolver implements IAnnotationResolver<Class<?>>{
+	
+	@Override
+	public void resolve(List<Class<?>> resolveList) {
+		
+	}
+}
