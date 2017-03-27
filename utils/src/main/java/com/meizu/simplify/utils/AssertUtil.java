0284f37e6cecdb1ee5e82bf0de101c12be4f091f
lcy <547393607@qq.com>
2017-03-27 13:56:59 +0800

    utils模块：规范AssertUtil的注释

--- utils/src/main/java/com/meizu/simplify/utils/AssertUtil.java
+++ utils/src/main/java/com/meizu/simplify/utils/AssertUtil.java
@@ -3,8 +3,8 @@ package com.meizu.simplify.utils;
 import com.meizu.simplify.exception.UncheckedException;
 
 /**
- * <p><b>Title:</b><i>断言工具类。该工具类常用于对参数进行校验</i></p>
- * <p>Desc: TODO</p>
+ * <p><b>Title:</b><i>断言工具类，该工具类常用于对参数进行校验</i></p>
+ * <p>Desc: 注意：这个工具类不同于Message工具类，Message是用于web项目中的信息提示，而AssertUtil不特定于项目类型，比如rpc服务端中也可使用,AssertUtil只用于数据校验</p>
  * <p>source folder:{@docRoot}</p>
  * <p>Copyright:Copyright(c)2014</p>
  * <p>Company:meizu</p>
@@ -15,14 +15,14 @@ import com.meizu.simplify.exception.UncheckedException;
  * @version Version 0.1
  *
  */
-public abstract class AssertUtil {
+public class AssertUtil {
+
 	/**
-	 * 断言表达式为true。
 	 * 
-	 * @param expression
-	 *            表达式
-	 * @param message
-	 *            错误信息
+	 * 方法用途: 断言表达式为true<br>
+	 * 操作步骤: TODO<br>
+	 * @param expression 表达式
+	 * @param message 错误信息
 	 */
 	public static void isTrue(Boolean expression, String message) {
 		if (!expression) {
@@ -31,22 +31,21 @@ public abstract class AssertUtil {
 	}
 
 	/**
-	 * 断言表达式为true。
 	 * 
-	 * @param expression
-	 *            表达式
+	 * 方法用途: 断言表达式为true<br>
+	 * 操作步骤: TODO<br>
+	 * @param expression 表达式
 	 */
 	public static void isTrue(Boolean expression) {
 		isTrue(expression, "断言失败: 表达式必须是true");
 	}
 
 	/**
-	 * 断言对象为null。
 	 * 
-	 * @param object
-	 *            待判断的对象
-	 * @param message
-	 *            错误信息
+	 * 方法用途: 断言对象为null<br>
+	 * 操作步骤: TODO<br>
+	 * @param object 待判断的对象
+	 * @param message 错误信息
 	 */
 	public static void isNull(Object object, String message) {
 		if (object != null) {
@@ -55,22 +54,21 @@ public abstract class AssertUtil {
 	}
 
 	/**
-	 * 断言对象为null。
 	 * 
-	 * @param object
-	 *            待判断的对象
+	 * 方法用途: 断言对象为null<br>
+	 * 操作步骤: TODO<br>
+	 * @param object 待判断的对象
 	 */
 	public static void isNull(Object object) {
 		isNull(object, "断言失败: 对象必须是null");
 	}
 
 	/**
-	 * 断言对象不为null。
 	 * 
-	 * @param object
-	 *            待判断的对象
-	 * @param message
-	 *            错误信息
+	 * 方法用途: 断言对象不为null<br>
+	 * 操作步骤: TODO<br>
+	 * @param object 待判断的对象
+	 * @param message 错误信息
 	 */
 	public static void notNull(Object object, String message) {
 		if (object == null) {
@@ -79,22 +77,21 @@ public abstract class AssertUtil {
 	}
 
 	/**
-	 * 断言对象不为null。
 	 * 
-	 * @param object
-	 *            待判断的对象
+	 * 方法用途: 断言对象不为null<br>
+	 * 操作步骤: TODO<br>
+	 * @param object 待判断的对象
 	 */
 	public static void notNull(Object object) {
 		notNull(object, "断言失败: 对象不能是null");
 	}
 
 	/**
-	 * 断言字符串为空。
 	 * 
-	 * @param text
-	 *            字符串
-	 * @param message
-	 *            错误信息
+	 * 方法用途: 断言字符串为空<br>
+	 * 操作步骤: TODO<br>
+	 * @param text 字符串
+	 * @param message 错误信息
 	 */
 	public static void isEmpty(String text, String message) {
 		if (StringUtil.isNotEmpty(text)) {
@@ -103,22 +100,21 @@ public abstract class AssertUtil {
 	}
 
 	/**
-	 * 断言字符串为空。
 	 * 
-	 * @param text
-	 *            字符串
+	 * 方法用途: 断言字符串为空<br>
+	 * 操作步骤: TODO<br>
+	 * @param text 字符串
 	 */
 	public static void isEmpty(String text) {
 		isEmpty(text, "断言失败: 字符串必须是空");
 	}
 
 	/**
-	 * 断言字符串不为空。
 	 * 
-	 * @param text
-	 *            字符串
-	 * @param message
-	 *            错误信息
+	 * 方法用途: 断言字符串不为空<br>
+	 * 操作步骤: TODO<br>
+	 * @param text 字符串
+	 * @param message 错误信息
 	 */
 	public static void notEmpty(String text, String message) {
 		if (StringUtil.isEmpty(text)) {
