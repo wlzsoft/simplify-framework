cc7b2fdb48856b3bea48835654d21285a0b68b31
wlz <wanglizong@meizu.com>
2017-09-07 10:39:34 +0800

    修改单元测试异常

--- dao/src/test/java/vip/simplify/dao/DaoTest.java
+++ dao/src/test/java/vip/simplify/dao/DaoTest.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-import com.sun.xml.internal.rngom.parse.host.Base;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
