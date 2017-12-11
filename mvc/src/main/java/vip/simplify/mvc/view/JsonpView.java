f1c46f70adaaefa056ed0590df4441869277e915
wlz <wanglizong@meizu.com>
2017-12-11 16:14:34 +0800

    修改XSS

--- mvc/src/main/java/vip/simplify/mvc/view/JsonpView.java
+++ mvc/src/main/java/vip/simplify/mvc/view/JsonpView.java
@@ -61,7 +61,8 @@ public  class  JsonpView {
 				model.setCallback("jsonp");
 			}
 			//message = StringUtil.format("{0}({1})", model.getCallback(), message);
-			message = StringUtil.format("<script>{0}({1});</script>", model.getCallback(), message);
+			//String callback = model.getCallback().replaceAll("script", "");
+			//message = StringUtil.format("<script>{0}({1});</script>", model.getCallback(), message);
 		}
 		response.setCharacterEncoding(config.getCharset());
 		response.setContentType("text/html; charset=" + config.getCharset());
