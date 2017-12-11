8e5160c9e5deeaf7fbb94641b6dddb8a90217fc5
wlz <wanglizong@meizu.com>
2017-12-11 17:04:26 +0800

    修改XSS快脚本攻击过滤

--- mvc/src/main/java/vip/simplify/mvc/view/JsonpView.java
+++ mvc/src/main/java/vip/simplify/mvc/view/JsonpView.java
@@ -60,6 +60,8 @@ public  class  JsonpView {
 			if(null==model.getCallback()||model.getCallback().equals("")){
 				model.setCallback("jsonp");
 			}
+			String callback = model.getCallback().replaceAll("script", "");
+			message = StringUtil.format("{0}({1})", callback, message);
 			//message = StringUtil.format("{0}({1})", model.getCallback(), message);
 			//String callback = model.getCallback().replaceAll("script", "");
 			//message = StringUtil.format("<script>{0}({1});</script>", model.getCallback(), message);
